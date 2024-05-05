d005d37ab2b32e3b397e0a584d6aab8ad396e75d
aisevim <a.aydnlp.pro@gmail.com>
2024-05-05 14:42:10 +1000

    feat(it-VA): add it-VA support

---
+++ src/langs/it-VA.js
@@ -0,0 +1,11 @@
+export function getText(options = {}) {
+  const {
+    suffix,
+  } = options
+
+  return {
+    text: `La mattina sorge sulla Città del Vaticano, illuminando la maestosità della Basilica di San Pietro. I fedeli si radunano in Piazza San Pietro per assistere alla benedizione papale.${ suffix }Padre Giovanni, un residente del Vaticano, si prepara per celebrare la messa mattutina presso una delle cappelle della Città del Vaticano.${ suffix }Le guardie svizzere in uniforme colorata pattugliano le mura, simbolo della protezione e della sicurezza della Città del Vaticano.${ suffix }I visitatori provenienti da tutto il mondo si riuniscono per esplorare i musei vaticani, ammirando capolavori dell'arte e della storia.${ suffix }I cardinali e i vescovi si incontrano per discutere questioni ecclesiastiche e prendere decisioni importanti per la Chiesa.${ suffix }Le suore e i frati vaticani si dedicano alla preghiera e al servizio ai pellegrini, offrendo conforto e sostegno spirituale.${ suffix }I giardini vaticani offrono un'oasi di pace e tranquillità, con splendide aiuole di fiori e alberi secolari.${ suffix }I visitatori possono assistere alla cerimonia dell'Angelus nella piazza, ascoltando le parole del Santo Padre e pregando insieme.${ suffix }I camerieri del Vaticano preparano pasti per i dignitari ecclesiastici e i visitatori illustri, seguendo antiche tradizioni culinarie.${ suffix }Nel pomeriggio, i fedeli si recano alla tomba di San Pietro per pregare e riflettere.${ suffix }I giornalisti dei media vaticani lavorano per diffondere notizie e informazioni sulle attività della Santa Sede.${ suffix }I fedeli partecipano alla processione del Corpus Domini, portando con devozione l'Eucaristia per le strade della Città del Vaticano.${ suffix }Le suore missionarie vaticane si recano nei quartieri circostanti per offrire aiuto e assistenza ai più bisognosi.${ suffix }I funzionari vaticani si occupano della gestione amministrativa e logistica della Città del Vaticano, garantendo il suo corretto funzionamento.${ suffix }`,
+    code: `it-VA`,
+    macro: `it`,
+  }
+}
